3ed70ffd1e0f561e17aae517fd7ed212fcc90214
mahendrasinghmanikandan <77800672+mahendrasinghmanikandan@users.noreply.github.com>
2021-03-18 12:13:51 +0530

    Update ArrayMethods.js

--- Daily-Status/March_17-03-2021/ArrayMethods.js
+++ Daily-Status/March_17-03-2021/ArrayMethods.js
@@ -11,7 +11,7 @@ console.log("toString :"+A.toString());
 //Join
 
 var B = ["Banana", "Orange", "Apple", "Mango"];
-console.log("Join :"+B.join("*"));
+console.log("Join :"+B.join(":"));
 
 //pop
 
@@ -21,7 +21,7 @@ console.log("pop :" + C.pop()+"  Balance :"+C);
 //push
 
 var D = ["Banana", "Orange", "Apple", "Mango"];
-console.log("push :" + D.push("Kiwi")+"  Total :"+D);
+console.log("push :" + D.push("msm")+"  Total :"+D);
 
 //shift
 
@@ -31,10 +31,7 @@ console.log("shift :" + E.shift()+"  Balance :"+E);
 //unshift
 
 var F = ["Banana", "Orange", "Apple", "Mango"];
-console.log("unshift :" + F.unshift("Kiwi") + "  Total :" + F);
-
-
-
+console.log("unshift :" + F.unshift("msm") + "  Total :" + F);
 
 //splice
 
@@ -46,3 +43,24 @@ console.log("splice :" + G.splice(1,0,"msm") + "  Total :" + G);
 
 var  H= ["Banana", "Orange", "Apple", "Mango"];
 console.log("slice :" + H.slice(1) + "  Total :" + H);
+
+
+//merge
+
+var I1 = ["Banana1", "Orange1", "Apple1", "Mango1"];
+var I2 = ["Banana2", "Orange2", "Apple2", "Mango2"];
+var I3 = I1.concat(I2);
+console.log("array_1 :" + I1, "\n array_2 :" + I2 + "\n merge_array :" + I3);
+
+//delete
+
+var  J= ["Banana", "Orange", "Apple", "Mango"];
+console.log("delete 0'th:" + delete J[0] + "  Balance :" + J);
+
+//Changing Elements
+
+var  k= ["Banana", "Orange", "Apple", "Mango"];
+
+k[0] = 1;
+
+console.log("Changing Elements :" + k);
