4c81bff49bf2645bd2cb712c0fb3f56d04c28208
mahendrasinghmanikandan <77800672+mahendrasinghmanikandan@users.noreply.github.com>
2021-03-18 15:54:24 +0530

    Update Task1.js

--- Daily-Status/March_17-03-2021/Task1.js
+++ Daily-Status/March_17-03-2021/Task1.js
@@ -1,5 +1,8 @@
 let a = [1, '1', 'Name', [1, 2], true];
 
-a.forEach(data => {
-    console.log("a[" + data + "]:is " + typeof (data));
+
+a.forEach((data,index) => {
+    a[index] = typeof (data);
 })
+
+console.log(a);
