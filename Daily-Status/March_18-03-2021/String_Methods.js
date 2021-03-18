2123b3528b6bbc0f92fdf665254a24564cf95090
mahendrasinghmanikandan <77800672+mahendrasinghmanikandan@users.noreply.github.com>
2021-03-18 19:12:41 +0530

    StringMethods

---
+++ Daily-Status/March_18-03-2021/String_Methods.js
@@ -0,0 +1,15 @@
+//Length
+var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+console.log(alphabet+"-> Length :"+alphabet.length);
+
+//indexOf vs lastIndexOf
+
+var str = "ABCA";
+console.log("indexOf vs lastIndexOf :"+str);
+console.log("indexof(A) :" + str.indexOf("A"));
+console.log("lastIndexOf(A) :" + str.lastIndexOf("A"));
+
+console.log("indexof(A,1) :" + str.indexOf("A",1));
+console.log("lastIndexOf(A,1) :"+str.lastIndexOf("A",1));
+
