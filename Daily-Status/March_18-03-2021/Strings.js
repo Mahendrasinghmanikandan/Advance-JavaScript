500cd9aa0ddddfa06b1302cc1c5c52e2e35c39c1
mahendrasinghmanikandan <77800672+mahendrasinghmanikandan@users.noreply.github.com>
2021-03-18 18:16:04 +0530

    Strings

---
+++ Daily-Status/March_18-03-2021/Strings.js
@@ -0,0 +1,17 @@
+var string1 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+console.log("string :" + string1);
+
+//length
+console.log("length :" + string1.length);
+
+//Escape Character
+
+console.log("Single quote :" + 'It\'s alright.');
+console.log("Double quote :" + "\"Better late than never.\"");
+console.log("Backslash :" + "x\\y");
+
+//string object's
+
+var string2 = new String("msm");
+console.log(string2);
