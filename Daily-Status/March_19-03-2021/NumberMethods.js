390a0e458001cb3bc69dd6ce2be00cb63da8df00
mahendrasinghmanikandan <77800672+mahendrasinghmanikandan@users.noreply.github.com>
2021-03-22 12:59:27 +0530

    Add files via upload

---
+++ Daily-Status/March_19-03-2021/NumberMethods.js
@@ -0,0 +1,1 @@
+console.log('NumberMethods_output');
---
+++ Daily-Status/March_19-03-2021/Numbers.js
@@ -0,0 +1,28 @@
+//Numbers
+console.log('1   :'+1);
+console.log('1.2   :'+1.2);
+console.log('\'1\'   :'+'1');
+console.log('1e4   :'+1e4);
+console.log('1e-1   :'+1e-1);
+console.log(111111111111111);
+
+console.log(1 + 1);
+console.log('1' + '1');
+console.log(1 + '1');
+console.log(1 * '10');
+console.log(1 - '10');
+console.log(10 / '1');
+console.log(1 + 'A');
+console.log(1 / 'A');
+
+//Infinity vs -Infinity
+
+console.log("Infinity"+1 / 0);
+console.log("-Infinity" + -1 / 0);
+
+var myNumber = 32;
+console.log(myNumber.toString(10));
+console.log(myNumber.toString(32));
+console.log(myNumber.toString(16));
+console.log(myNumber.toString(8)); 
+console.log(myNumber.toString(2)); 
