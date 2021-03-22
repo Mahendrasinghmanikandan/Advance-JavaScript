2397fa0378d7fd8d9c8993d95bd3ce53e88d7ee2
mahendrasinghmanikandan <77800672+mahendrasinghmanikandan@users.noreply.github.com>
2021-03-22 13:18:58 +0530

    Update NumberMethods.js

--- Daily-Status/March_19-03-2021/NumberMethods.js
+++ Daily-Status/March_19-03-2021/NumberMethods.js
@@ -1,1 +1,23 @@
-console.log('NumberMethods_output');
+//toString()
+
+console.log((5).toString());
+
+//toExponential() 
+
+console.log((555).toExponential(1));
+
+//toFixed(0)
+
+console.log((99.99).toFixed());
+
+// toPrecision()
+
+
+console.log((9999).toPrecision(4));
+
+
+//Number() vs parseFloat() vs parseInt()
+
+console.log(Number("10"));
+console.log(parseFloat("10.5 yar 10"));
+console.log(parseInt("yar 10"));
