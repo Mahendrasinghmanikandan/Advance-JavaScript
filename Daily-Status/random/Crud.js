cf10237ef97ae89a3829f7a2d2411678aca5ff88
mahendrasinghmanikandan <77800672+mahendrasinghmanikandan@users.noreply.github.com>
2021-04-15 14:35:11 +0530

    Add files via upload

---
+++ Daily-Status/random/Crud.js
@@ -0,0 +1,42 @@
+import { React, useState } from 'react'
+
+function Crud() {
+    const [names, setnames] = useState([
+        {
+            id: 1,
+            name: "nesrin"
+        }
+    ])
+    const [name, setname] = useState()
+
+    const Add = (event) => {
+        event.preventDefault()
+        let id = Math.floor(Math.random() * 1000) + 1;
+        let newdata = { id, name }
+        // console.log(newdata)
+        setnames([...names, newdata])
+    }
+    return (
+        <div>
+            <form onSubmit={Add}>
+                <input type="text" placeholder="enter Name Here" value={name} onChange={(e) => { setname(e.target.value) }} />
+                <input type="submit" />
+            </form>
+            <table border='1px' cellPadding='25%'>
+                <tr>
+                    <td>S.No</td>
+                    <td>Names</td>
+                </tr>
+                {names.map((e) => (
+                    <tr key={e.id}>
+                        <td>{e.id}</td>
+                        <td>{e.name}</td>
+                    </tr>
+                ))}
+
+            </table>
+        </div>
+    )
+}
+
+export default Crud
