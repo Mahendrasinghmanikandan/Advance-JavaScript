7f7e8e1a7dd930b2961a0429170148a876704252
mahendrasinghmanikandan <77800672+mahendrasinghmanikandan@users.noreply.github.com>
2021-04-30 00:52:49 +0530

    Drawer

--- Daily-Status/random/Dev.js
+++ Daily-Status/random/Dev.js
@@ -1,6 +1,6 @@
 import { React, useState, useEffect } from 'react'
 import axios from 'axios'
-import { Table, Input, Space, Row, Col, Modal } from 'antd';
+import { Table, Input, Space, Row, Col, Form, Drawer, Button } from 'antd';
 import { DeleteOutlined, EditOutlined, UserAddOutlined } from '@ant-design/icons'
 const { Search } = Input;
 const Dev = () => {
@@ -12,9 +12,9 @@ const Dev = () => {
     const [body, setbody] = useState()
     // for search
     const [search, setsearch] = useState('')
-    // Models
-    const [isEditModalVisible, setisEditModalVisible] = useState(false);
-    const [isAddModalVisible, setisAddModalVisible] = useState(false);
+    // Drawer
+    const [isEditDrawerVisible, setisEditDrawerVisible] = useState(false);
+    const [isAddDrawerVisible, setisAddDrawerVisible] = useState(false);
     // get data from api
     useEffect(() => {
         axios.get('https://jsonplaceholder.typicode.com/posts').then(res => {
@@ -38,7 +38,7 @@ const Dev = () => {
     const columns = [
         {
             title: 'S.No',
-            dataIndex: 'sno',
+            dataIndex: search ? 'id' : 'sno',
             key: 'id',
         },
         {
@@ -60,7 +60,7 @@ const Dev = () => {
                         setbody(record.body)
                         settitle(record.title)
 
-                    }}><EditOutlined onClick={() => { setisEditModalVisible(true) }} /></p>
+                    }}><EditOutlined onClick={() => { setisEditDrawerVisible(true) }} /></p>
                     <p style={{ cursor: 'pointer', color: 'red' }} onClick={() => {
                         setdatas(datas.filter(res => res.id !== record.id))
                     }}><DeleteOutlined /></p>
@@ -71,12 +71,17 @@ const Dev = () => {
     ];
     // Update 
     const handleUpdate = () => {
-        datas.map(res => (
-            res.id === uid ? (res.title = title, res.body = body) : (res.title = res.title, res.body = res.body)
-        ))
-        settitle('')
-        setuid('')
-        setbody('')
+        if (title !== '' && body !== '') {
+            datas.map(res => (
+                res.id === uid ? (res.title = title, res.body = body) : (res.title = res.title, res.body = res.body)
+            ))
+            settitle('')
+            setbody('')
+            setuid('')
+            setisEditDrawerVisible(false)
+        } else {
+            alert('hey Fill the details')
+        }
     }
     // Add
     const handleAdd = () => {
@@ -86,43 +91,96 @@ const Dev = () => {
             setdatas([...datas, newdata])
             settitle('')
             setbody('')
+            setisAddDrawerVisible(false)
         } else {
             alert('hey Fill the details')
         }
     }
     return (
         <div>
-            {/* Edit Model Start*/}
-            <Modal title="Update Datas" visible={isEditModalVisible} onOk={() => {
-                setisEditModalVisible(false)
-                handleUpdate()
-            }} onCancel={() => {
-                setisEditModalVisible(false)
+            {/* Edit Drawer Start*/}
+            <Drawer title="Update Datas" width={500} visible={isEditDrawerVisible} onClose={() => {
+                setisEditDrawerVisible(false)
                 settitle('')
                 setuid('')
                 setbody('')
-            }} >
-                <label>Title:</label>
-                <Input value={title} onChange={(e) => { settitle(e.target.value) }} size='large' /><br />
-                <label>Body:</label>
-                <Input value={body} onChange={(e) => { setbody(e.target.value) }} size='large' /><br />
-            </Modal>
-            {/* Edit Model End */}
-            {/* Add Model  Start*/}
-            <Modal title="Add Datas" visible={isAddModalVisible} onOk={() => {
-                setisAddModalVisible(false)
+            }}
+                footer={
+                    <div>
+                        <Button style={{ marginRight: 8 }} onClick={() => {
+                            setisEditDrawerVisible(false)
+                            settitle('')
+                            setuid('')
+                            setbody('')
+                        }}>
+                            Cancel
+                       </Button>
+                        <Button type="primary" onClick={() => { handleUpdate() }} >Update</Button>
+                    </div>
+                }>
+                <Form >
+                    <Row gutter={24}>
+                        <Col span={24}>
+                            <Form.Item label="Title">
+                                <Input value={title} onChange={(e) => { settitle(e.target.value) }} size='large' />
+                            </Form.Item>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col span={24}>
+                            <Form.Item label="Body">
+                                <Input
+                                    value={body} onChange={(e) => { setbody(e.target.value) }} size='large' />
+                            </Form.Item>
+                        </Col>
+                    </Row>
+                </Form>
+
+            </Drawer>
+            {/* Edit Drawer End */}
+            {/* Add Drawer Start*/}
+            <Drawer title="Add Datas" width={720} visible={isAddDrawerVisible} onOk={() => {
+                setisAddDrawerVisible(false)
                 handleAdd()
-            }} onCancel={() => {
-                setisAddModalVisible(false)
+            }} onClose={() => {
+                setisAddDrawerVisible(false)
                 settitle('')
                 setbody('')
-            }} >
-                <label>Title:</label>
-                <Input value={title} onChange={(e) => { settitle(e.target.value) }} size='large' /><br />
-                <label>Body:</label>
-                <Input value={body} onChange={(e) => { setbody(e.target.value) }} size='large' /><br />
-            </Modal>
-            {/* Add Model End */}
+            }}
+                footer={
+                    <div>
+                        <Button style={{ marginRight: 8 }} onClick={() => {
+                            setisAddDrawerVisible(false)
+                            settitle('')
+                            setbody('')
+                        }}>
+                            Cancel
+                       </Button>
+                        <Button type="primary" onClick={() => { handleAdd() }} >Add</Button>
+                    </div>
+                }>
+                <Form >
+                    <Row gutter={24}>
+                        <Col span={24}>
+                            <Form.Item label="Title">
+                                <Input value={title} onChange={(e) => { settitle(e.target.value) }} size='large' />
+                            </Form.Item>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col span={24}>
+                            <Form.Item label="Body">
+                                <Input
+                                    value={body} onChange={(e) => { setbody(e.target.value) }} size='large' />
+                            </Form.Item>
+
+                        </Col>
+                    </Row>
+                </Form>
+
+            </Drawer>
+            {/* Add Drawer End */}
+
             <Row justify='center'>
                 <Col span={18}>
                     <Search style={{ marginTop: '2%' }}
@@ -132,7 +190,7 @@ const Dev = () => {
                         size="large"
                         onChange={(e) => { setsearch(e.target.value) }}
                     />
-                    <UserAddOutlined onClick={() => { setisAddModalVisible(true) }} style={{ float: 'right', color: 'green', margin: '2%', cursor: 'pointer' }} />
+                    <UserAddOutlined onClick={() => { setisAddDrawerVisible(true) }} style={{ float: 'right', color: 'green', margin: '2%', cursor: 'pointer' }} />
                 </Col>
             </Row>
             {/* Table Start*/}
